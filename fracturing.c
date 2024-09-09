725934453596ded1e7a7f2e9f3dc06a7a3a70aaf
dilla5469 <dillaerick5469@gmail.com>
2024-09-09 02:51:25 +0000

    read the comments.

--- fracturing.c
+++ fracturing.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <math.h>
+#define PI = 3.14159
 
-int main() {
+//I genuinely tried to do this assignment plenty of times, i started it various times throughout the week and have spent hours reviewing the material but its too much
+//The various tweaks made to the submission, as well as having to update the repo, just made it way too confusing.
+//It is not ideal to have 5 different pages open each with unique instructions as well as the lecture recordings for reference as well as my codespaces.
+//I just recovered from a panic attack due to the approaching deadline and how lost i felt doing this assignment
+//and came to the conclusion that for my mental health i can't finish this tonight.
 
 
+int main(int argc, char **argv) ;
+{
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
 
     return 0;
 }
+int helper();
+{
+    int x1 = 1;
+    int x2 = 2;
+    int y1 = 3;
+    int y2 = 4;
+    calculateDistance = 
+}
+double calculateDistance();
+{
+    
+
+
+
+}
+
+double calculatePerimeter();
+{
+
+}
+
+double calculateArea();
+{
+
+}
+
+double calculateWidth();
+{
+
+
+}
+
+double calculateHeight();
+{
+
+}
+
