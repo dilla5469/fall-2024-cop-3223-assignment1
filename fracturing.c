833ac2548d5f1f5ce37342f4cbf01e3303163ef5
dilla5469 <dillaerick5469@gmail.com>
2024-09-10 22:37:44 +0000

    done done

--- fracturing.c
+++ fracturing.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #define PI 3.14159
 
-double getDistance(double x1, double x2, double y1, double y2){
+double getDistance(){
     double x1 = 0;
     double x2 = 0;
     double y1 = 0;
@@ -19,6 +19,8 @@ double calculateDistance(){
     double x2 = 0;
     double y1 = 0;
     double y2 = 0;
+    printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     printf("The distance between the two points is %lf\n", distance);
     
@@ -30,6 +32,8 @@ double calculatePerimeter(){
     double x2 = 0;
     double y1 = 0;
     double y2 = 0;
+    printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     double radius = getDistance() / 2;
     double perimeter = 2 * PI * radius;
     printf("The perimeter of the city encompassed by your request is %lf\n", perimeter);
@@ -42,6 +46,8 @@ double calculateArea(){
     double x2 = 0;
     double y1 = 0;
     double y2 = 0;
+    printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     double radius = getDistance() / 2;
     double area = PI * pow(radius, 2);
     printf("The area of the city encompassed by your request is %lf\n", area);
@@ -55,6 +61,8 @@ double calculateWidth(){
     double y1 = 0;
     double y2 = 0;
     double width = getDistance();
+    printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     printf(" The width of the city encompassed by your request is %lf\n", width);
     
     return 1.0;
@@ -65,6 +73,8 @@ double calculateHeight(){
     double x2 = 0;
     double y1 = 0;
     double y2 = 0;
+    printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
+    printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     double height = getDistance();
     printf("The height of the city encompassed by your request is %lf\n", height);
     
